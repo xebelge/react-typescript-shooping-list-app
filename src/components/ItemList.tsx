934b94b369a353774d24f90b05c1c3be2644b6a7
Eda Belge <edabelge00@gmail.com>
2023-08-24 17:23:41 +0300

    Item adding without category fixed

--- src/components/ItemList.tsx
+++ src/components/ItemList.tsx
@@ -17,15 +17,19 @@ interface CategorizedItems {
 const ItemList: React.FC<ItemListProps> = ({ items, onAddFavorite, onRemove, onAddToCart, onRemoveCategory, categories }) => {
     const categorizedItems: CategorizedItems = {};
 
+    // Separate items into categories
     items.forEach(item => {
         const category = item.category || 'Uncategorized';
         categorizedItems[category] = categorizedItems[category] || [];
         categorizedItems[category].push(item);
     });
 
+    const uncategorizedItems = categorizedItems['Uncategorized'] || [];
+
     return (
         <div className="item-list">
             <h2>Shopping List</h2>
+            {/* Display items with categories */}
             {categories.map(category => (
                 <ul key={category} className="category-list">
                     <li className="category-item">
@@ -46,6 +50,23 @@ const ItemList: React.FC<ItemListProps> = ({ items, onAddFavorite, onRemove, onA
                     </li>
                 </ul>
             ))}
+
+            {/* Display items without a category */}
+            <ul key="uncategorized" className="category-list">
+                <li className="category-item">
+                    <h3>Other</h3>
+                    <ul>
+                        {uncategorizedItems.map((item, index) => (
+                            <li key={index}>
+                                <Item {...item} />
+                                <button onClick={() => onAddFavorite(item)}>Add to Favorites</button>
+                                <button onClick={() => onRemove(index)}>Remove</button>
+                                <button onClick={() => onAddToCart(item)}>Add Cart</button>
+                            </li>
+                        ))}
+                    </ul>
+                </li>
+            </ul>
         </div>
     );
 };
